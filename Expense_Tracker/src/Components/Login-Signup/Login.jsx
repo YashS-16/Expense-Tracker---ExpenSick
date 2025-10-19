c25539bd815ddfb540bfac3e23a1c8163091ab0c
Yash Sangale <yashsangale16@gmail.com>
2025-10-19 12:24:13 +0530

    Updated Changes

---
+++ Expense_Tracker/src/Components/Login-Signup/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <>Login to ExpenSick
+        <div className="LoginForm">
+            <div className="login-con">
+                <h2>Login</h2>
+                <form action="">
+                    
+                </form>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Login
